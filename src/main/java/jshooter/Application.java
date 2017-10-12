b82a777eac835f4c8cbb4f7f38435ae62f2f6d08
Marvin Bruns <android.bruns@googlemail.com>
2017-10-12 21:28:37 +0200

    initializing app

--- src/main/java/jshooter/Application.java
+++ src/main/java/jshooter/Application.java
@@ -1,5 +1,8 @@
 package jshooter;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +12,13 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 import com.github.brunothg.swing.mvp.annotation.EnableSwingMVP;
 
+/**
+ * Application starting point. Setting up spring-context and other initial
+ * stuff.
+ * 
+ * @author Marvin Bruns
+ *
+ */
 @SpringBootApplication
 @EnableSwingMVP
 public class Application {
@@ -20,6 +30,21 @@ public class Application {
 	public static void main(String[] args) {
 		LOG.info("Launching application ...");
 
+		initializeSpring(args);
+		initializeUi();
+	}
+
+	private static void initializeUi() {
+		LOG.info("Setting LookAndFeel to system default");
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException
+				| IllegalAccessException | UnsupportedLookAndFeelException e) {
+			LOG.warn("Could not set LookAndFeel to system default", e);
+		}
+	}
+
+	private static void initializeSpring(String[] args) {
 		ctx = new SpringApplicationBuilder(Application.class).headless(false)
 				.web(false).run(args);
 	}
