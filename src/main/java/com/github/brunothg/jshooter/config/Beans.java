e1303efe55f4da42f63bd3277f1d62c85a29de92
Marvin Bruns <android.bruns@googlemail.com>
2017-10-25 12:15:44 +0200

    language settings

--- src/main/java/com/github/brunothg/jshooter/config/Beans.java
+++ src/main/java/com/github/brunothg/jshooter/config/Beans.java
@@ -1,5 +1,8 @@
 package com.github.brunothg.jshooter.config;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,11 +12,13 @@ import com.github.brunothg.jshooter.utils.I18N;
 public class Beans {
 
 	@Bean(name = { "i18n" })
-	public I18N getI18N() {
+	@Autowired
+	public I18N getI18N(UserProperties userSettings) {
 		I18N i18n = new I18N();
 		i18n.setBaseName("languages.language");
 		i18n.setGetKeyWehnMissing(true);
 		i18n.setClassLoader(null);
+		i18n.setDefaultLocale(Locale.forLanguageTag(userSettings.getLanguage()));
 
 		return i18n;
 	}
